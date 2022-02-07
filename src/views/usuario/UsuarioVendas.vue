c95d4964273d5a7b67988fa050eebfcfd1eeb794
Elias Barbosa <69683964+EliasBarbosa214@users.noreply.github.com>
2022-02-07 15:14:51 -0300

    Compras e Vendas || Final do Projeto

--- src/views/usuario/UsuarioVendas.vue
+++ src/views/usuario/UsuarioVendas.vue
@@ -1,9 +1,93 @@
 <template>
-  <p>Lista de Vendas</p>
+  <section>
+    <div v-if="vendas">
+      <h2>Vendas</h2>
+      <div
+        class="produtos-wrapper"
+        v-for="(venda, index) in vendas"
+        :key="index"
+      >
+        <ProdutoItem v-if="venda.produto" :produto="venda.produto"
+          ><p class="comprador">
+            <span>Comprador:</span> {{ venda.comprador_id }}
+          </p>
+        </ProdutoItem>
+        <div class="entrega">
+          <h3>Entrega:</h3>
+          <ul v-if="venda.endereco">
+            <li v-for="(value, key) in venda.endereco" :key="key">
+              {{ key }}: {{ value }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import ProdutoItem from "@/components/ProdutoItem.vue";
+import { api } from "@/services.js";
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      vendas: null,
+    };
+  },
+  computed: {
+    ...mapState(["login", "usuario"]),
+  },
+  components: {
+    ProdutoItem,
+  },
+  methods: {
+    getVendas() {
+      api.get(`/transacao?comprador_id=${this.usuario.id}`).then((response) => {
+        this.vendas = response.data;
+      });
+    },
+  },
+
+  watch: {
+    login() {
+      this.getVendas();
+    },
+  },
+
+  created() {
+    if (this.login) {
+      this.getVendas();
+    }
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.produto-wrapper {
+  margin-bottom: 40px;
+}
+
+.comprador span {
+  color: #e80;
+  font-weight: bold;
+}
+
+.entrega {
+  display: grid;
+  grid-template-columns: minmax(100px, 200px) 1fr;
+  grid-gap: 20px;
+  margin-bottom: 60px;
+  margin-top: 30px;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+
+h3 {
+  margin: 0;
+  justify-self: end;
+}
+</style>
